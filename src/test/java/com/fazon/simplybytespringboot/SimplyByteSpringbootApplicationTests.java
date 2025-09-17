83d96f3b7c15416822d8b1a0ee2a2d6212377802
sathish <110395437+kaalijen@users.noreply.github.com>
2025-09-17 23:01:25 +0530

    Update SimplyByteSpringbootApplicationTests.java-new17-09-2025

--- src/test/java/com/fazon/simplybytespringboot/SimplyByteSpringbootApplicationTests.java
+++ src/test/java/com/fazon/simplybytespringboot/SimplyByteSpringbootApplicationTests.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SimplyByteSpringbootApplicationTests {
+public class TestController {
 
-    @Test
-    void contextLoads() {
+    @GetMapping
+    public String health() {
+        return "Hello everyone, Jenkins pipeline code is ready to be here ";
     }
-
-}
